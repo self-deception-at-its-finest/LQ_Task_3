279db050d4cdd07bed7084db3123f965d96c3426
Mykhail <its.astral.step@gmail.com>
2025-10-14 21:10:10 +0300

    added global timeout

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
       baseUrl: 'https://telnyx.com',
+      defaultCommandTimeout: 10000,
       viewportWidth: 1920,
       viewportHeight: 1080,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
