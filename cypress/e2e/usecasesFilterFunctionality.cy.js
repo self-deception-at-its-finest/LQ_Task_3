8615716c058403463b8c69b468385f735521ea85
Mykhail <its.astral.step@gmail.com>
2025-10-16 02:48:25 +0300

    added ignoring uncaught exception to usecases filter test

--- cypress/e2e/usecasesFilterFunctionality.cy.js
+++ cypress/e2e/usecasesFilterFunctionality.cy.js
@@ -3,6 +3,11 @@ import SolutionsPage from "../pages/solutions.page";
 
 describe('User Cases', () => {
     it('Verify that Search filter for user cases at solutions page works properly', () => {
+        Cypress.on('uncaught:exception', (err, runnable) => {
+
+            return false
+        })
+
         const solutionsPage = new SolutionsPage()
 
         solutionsPage.open()
